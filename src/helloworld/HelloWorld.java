5e95107234c29a27675a60a2282371815d4e73a1
Carlos del Cerro <carlosc7fuen@gmail.com>
2014-11-25 16:47:36 +0100

    Primer programa hola mundo

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * Primer programa en JAVA. Hola Mundo.
+ */
+package helloworld;
+
+/**
+ * @author karlos
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
